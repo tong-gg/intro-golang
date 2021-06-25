a5e7c7cb481232ecc4c9f170092881da89c71a07
Patipan Tong <patipan.bo@ku.th>
2021-06-25 19:38:45 +0700

    01-main

---
+++ 01-main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Gopher!!")
+	fmt.Printf("Hello %s!\n", "Gopher")
+	fmt.Printf("Next line\n")
+
+	fmt.Printf("Patipan, age: %d\n", 20)
+	fmt.Printf("%v : %v\n", "with %v we can use any type", 20)
+}
